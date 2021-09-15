675f5286349a89cc2b9912feb328d740a9309063
Grzegorz Skowinski <gskowinski@samsung.com>
2021-09-15 08:30:48 +0200

    Added missing patch file.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ SRC="$(pwd)/../ffmpeg-fork"
 BUILD_DEPS="n"
 ARCH=2
 
-#git clone https://github.com/SamsungDForum/ffmpeg-fork.git --branch ffmpeg-4.3.1 "$SRC"
+git clone https://github.com/SamsungDForum/ffmpeg-fork.git --branch ffmpeg-4.3.1 "$SRC"
 echo $ARCH | ./cross_compile_ffmpeg.sh --build-ffmpeg-static=n --build-ffmpeg-shared=y --disable-nonfree=y --prefer-stable=y --build-dependencies=$BUILD_DEPS --ffmpeg-source-dir="$SRC"
 
 for f in "$SRC/bin/*.dll"; do upx --best $f; done
